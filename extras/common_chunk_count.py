f77b292ed657864d539458af602974392dc349b9
iychoi <iychoi@email.arizona.edu>
2013-11-15 20:11:01 -0700

    Command Line arguments check

--- extras/common_chunk_count.py
+++ extras/common_chunk_count.py
@@ -3,52 +3,61 @@ import sys
 import hashlib
 
 #Usage: common_chunk_count.py filepath1 filepath2 [chunksize]
-
 CHUNK_SIZE = 1024
 
-f1 = open(sys.argv[1], 'rb')
-f2 = open(sys.argv[2], 'rb')
+def main():
+	if len(sys.argv) < 3:
+		print "Usage: common_chunk_count.py filepath1 filepath2 [chunksize]"
+		return
 
-if len(sys.argv) > 3:
-	CHUNK_SIZE = int(sys.argv[3])
+	f1 = open(sys.argv[1], 'rb')
+	f2 = open(sys.argv[2], 'rb')
 
-l1 = [];
-l2 = [];
+	if len(sys.argv) > 3:
+		CHUNK_SIZE = int(sys.argv[3])
 
-try:
-	while True:
-		chunk = f1.read(CHUNK_SIZE)
-		if not chunk:
-			break
-		l1 = l1 + [hashlib.sha1(chunk).hexdigest(),]
-finally:
-	f1.close
+	l1 = [];
+	l2 = [];
 
-try:
-	while True:
-		chunk = f2.read(CHUNK_SIZE)
-		if not chunk:
-			break
-		l2 = l2 + [hashlib.sha1(chunk).hexdigest(),]
-finally:
-	f2.close
+	try:
+		while True:
+			chunk = f1.read(CHUNK_SIZE)
+			if not chunk:
+				break
+			l1 = l1 + [hashlib.sha1(chunk).hexdigest(),]
+	finally:
+		f1.close
+
+	try:
+		while True:
+			chunk = f2.read(CHUNK_SIZE)
+			if not chunk:
+				break
+			l2 = l2 + [hashlib.sha1(chunk).hexdigest(),]
+	finally:
+		f2.close
 	
-print "No of chunks in", f1.name + ":" ,len(l1)
-print "No of chunks in", f2.name + ":" ,len(l2)
+	print "No of chunks in", f1.name + ":" ,len(l1)
+	print "No of chunks in", f2.name + ":" ,len(l2)
 
-common_hashes = [i for i in l1 if i in l2]
-print "No of common chunks:", len(common_hashes)
+	common_hashes = [i for i in l1 if i in l2]
+	print "No of common chunks:", len(common_hashes)
 
-#for i in range(len(l1)):
-#	if l1[i] in l2:
-#		sys.stdout.write('#')
-#	else:
-#		sys.stdout.write('_')
-#print 
-#for i in range(len(l2)):
-#	if l2[i] in l1:
-#		sys.stdout.write('#')
-#	else:
-#		sys.stdout.write('_')
-#print 
-#sys.stdout.flush()
+	#for i in range(len(l1)):
+	#	if l1[i] in l2:
+	#		sys.stdout.write('#')
+	#	else:
+	#		sys.stdout.write('_')
+	#print 
+	#for i in range(len(l2)):
+	#	if l2[i] in l1:
+	#		sys.stdout.write('#')
+	#	else:
+	#		sys.stdout.write('_')
+	#print 
+	#sys.stdout.flush()
+
+
+
+if __name__ == "__main__":
+    main()
