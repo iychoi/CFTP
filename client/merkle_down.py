e7ae74d6c72867b9e8ea08378bdaad66332a18fd
iychoi <iychoi@email.arizona.edu>
2013-11-17 16:32:07 -0700

    Add commandline args

--- client/merkle_down.py
+++ client/merkle_down.py
@@ -10,43 +10,53 @@ import binascii
 
 from ca import Chunk_Handler
 
-ca = Chunk_Handler()
+def download(ca, ftp, filename):
+    target_file = filename
+    local_target_file = os.path.abspath(target_file)
+    temp_file = "/tmp/client_download.tmp"
 
-ftp = cftplib.FTP()
-ftp.connect("localhost", 2121)
-ftp.login("user", "12345")
+    if os.path.lexists(local_target_file):
+        ca.build_cache(local_target_file)
 
-target_file = "sample_down.dat"
-local_target_file = os.path.abspath(target_file)
-temp_file = "/tmp/client_download.tmp"
-
-if os.path.lexists(local_target_file):
-    ca.build_cache(local_target_file)
-
-merkle_info = cftp_client.getmerkleinfo(ftp, target_file)
-if merkle_info == None:
-    print "file not exist"
-else:
-    merkle_infoarr = merkle_info.split(",")
-    height = int(merkle_infoarr[0])
-    roothash = merkle_infoarr[1]
-    print "retrieve merkle tree", "height:", height
-    hashes = cftp_client.collectmerkletree(ftp, ca, target_file, height, roothash)
-    
-    if hashes == None:
+    merkle_info = cftp_client.getmerkleinfo(ftp, target_file)
+    if merkle_info == None:
         print "file not exist"
     else:
-        dest = open(temp_file, 'wb')
-        ret = cftp_client.collectchunks(ftp, ca, hashes, dest)
-        dest.close()
-
-        if ret:
-            if os.path.lexists(local_target_file):
-                os.remove(local_target_file)
-            os.rename(temp_file, local_target_file)
-            print "download complete"
-            ca.build_cache(local_target_file)
+        merkle_infoarr = merkle_info.split(",")
+        height = int(merkle_infoarr[0])
+        roothash = merkle_infoarr[1]
+        print "retrieve merkle tree", "height:", height
+        hashes = cftp_client.collectmerkletree(ftp, ca, target_file, height, roothash)
+        
+        if hashes == None:
+	        print "file not exist"
         else:
-            if os.path.lexists(temp_file):
-                os.remove(temp_file)
-            print "download failed"
+	        dest = open(temp_file, 'wb')
+	        ret = cftp_client.collectchunks(ftp, ca, hashes, dest)
+	        dest.close()
+
+	        if ret:
+	            if os.path.lexists(local_target_file):
+	                os.remove(local_target_file)
+	            os.rename(temp_file, local_target_file)
+	            print "download complete"
+	            ca.build_cache(local_target_file)
+	        else:
+	            if os.path.lexists(temp_file):
+	                os.remove(temp_file)
+	            print "download failed"
+
+def main():
+    if len(sys.argv) < 2:
+        print "command : ./merkle_down.py filename"
+    else:
+        filename = sys.argv[1]
+        ca = Chunk_Handler()
+
+        ftp = cftplib.FTP()
+        ftp.connect("localhost", 2121)
+        ftp.login("user", "12345")
+        download(ca, ftp, filename)
+
+if __name__ == "__main__":
+    main()
--- client/recipe_down.py
+++ client/recipe_down.py
@@ -10,34 +10,44 @@ import binascii
 
 from ca import Chunk_Handler
 
-ca = Chunk_Handler()
+def download(ca, ftp, filename):
+    target_file = filename
+    local_target_file = os.path.abspath(target_file)
+    temp_file = "/tmp/client_download.tmp"
 
-ftp = cftplib.FTP()
-ftp.connect("localhost", 2121)
-ftp.login("user", "12345")
-
-target_file = "sample_down.dat"
-local_target_file = os.path.abspath(target_file)
-temp_file = "/tmp/client_download.tmp"
-
-if os.path.lexists(local_target_file):
-    ca.build_cache(local_target_file)
-
-hashes = cftp_client.getrecipe(ftp, target_file)
-if hashes == None:
-    print "file not exist"
-else:
-    dest = open(temp_file, 'wb')
-    ret = cftp_client.collectchunks(ftp, ca, hashes, dest)
-    dest.close()
-    
-    if ret:
-        if os.path.lexists(local_target_file):
-            os.remove(local_target_file)
-        os.rename(temp_file, local_target_file)
-        print "download complete"
+    if os.path.lexists(local_target_file):
         ca.build_cache(local_target_file)
+
+    hashes = cftp_client.getrecipe(ftp, target_file)
+    if hashes == None:
+        print "file not exist"
     else:
-        if os.path.lexists(temp_file):
-            os.remove(temp_file)
-        print "download failed"
+        dest = open(temp_file, 'wb')
+        ret = cftp_client.collectchunks(ftp, ca, hashes, dest)
+        dest.close()
+        
+        if ret:
+            if os.path.lexists(local_target_file):
+                os.remove(local_target_file)
+            os.rename(temp_file, local_target_file)
+            print "download complete"
+            ca.build_cache(local_target_file)
+        else:
+            if os.path.lexists(temp_file):
+                os.remove(temp_file)
+            print "download failed"
+
+def main():
+    if len(sys.argv) < 2:
+        print "command : ./recipe_down.py filename"
+    else:
+        filename = sys.argv[1]
+        ca = Chunk_Handler()
+
+        ftp = cftplib.FTP()
+        ftp.connect("localhost", 2121)
+        ftp.login("user", "12345")
+        download(ca, ftp, filename)
+
+if __name__ == "__main__":
+    main()
