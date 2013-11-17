f1ae1e64efc084064ad9bbe79ef005b2afe15b6d
iychoi <iychoi@email.arizona.edu>
2013-11-17 16:36:01 -0700

    Introduced command line args

--- client/merkle_recipe.py
+++ client/merkle_recipe.py
@@ -10,30 +10,40 @@ import binascii
 
 from ca import Chunk_Handler
 
-ca = Chunk_Handler()
+def check(ca, ftp, filename):
+    target_file = filename
+    local_target_file = os.path.abspath(target_file)
+    temp_file = "/tmp/client_download.tmp"
 
-ftp = cftplib.FTP()
-ftp.connect("localhost", 2121)
-ftp.login("user", "12345")
+    if os.path.lexists(local_target_file):
+        ca.build_cache(local_target_file)
 
-target_file = "sample_down.dat"
-local_target_file = os.path.abspath(target_file)
-temp_file = "/tmp/client_download.tmp"
-
-if os.path.lexists(local_target_file):
-    ca.build_cache(local_target_file)
-
-merkle_info = cftp_client.getmerkleinfo(ftp, target_file)
-if merkle_info == None:
-    print "file not exist"
-else:
-    merkle_infoarr = merkle_info.split(",")
-    height = int(merkle_infoarr[0])
-    roothash = merkle_infoarr[1]
-    print "retrieve merkle tree", "height:", height
-    hashes = cftp_client.collectmerkletree(ftp, ca, target_file, height, roothash)
-    
-    if hashes == None:
+    merkle_info = cftp_client.getmerkleinfo(ftp, target_file)
+    if merkle_info == None:
         print "file not exist"
     else:
-	print hashes
+        merkle_infoarr = merkle_info.split(",")
+        height = int(merkle_infoarr[0])
+        roothash = merkle_infoarr[1]
+        print "retrieve merkle tree", "height:", height
+        hashes = cftp_client.collectmerkletree(ftp, ca, target_file, height, roothash)
+        
+        if hashes == None:
+            print "file not exist"
+        else:
+	        print hashes
+
+def main():
+    if len(sys.argv) < 2:
+        print "command : ./merkle_recipe.py filename"
+    else:
+        filename = sys.argv[1]
+        ca = Chunk_Handler()
+
+        ftp = cftplib.FTP()
+        ftp.connect("localhost", 2121)
+        ftp.login("user", "12345")
+        check(ca, ftp, filename)
+
+if __name__ == "__main__":
+    main()
--- client/recipe.py
+++ client/recipe.py
@@ -10,21 +10,31 @@ import binascii
 
 from ca import Chunk_Handler
 
-ca = Chunk_Handler()
+def check(ca, ftp, filename):
+    target_file = filename
+    local_target_file = os.path.abspath(target_file)
+    temp_file = "/tmp/client_download.tmp"
 
-ftp = cftplib.FTP()
-ftp.connect("localhost", 2121)
-ftp.login("user", "12345")
+    if os.path.lexists(local_target_file):
+        ca.build_cache(local_target_file)
 
-target_file = "sample_down.dat"
-local_target_file = os.path.abspath(target_file)
-temp_file = "/tmp/client_download.tmp"
+    hashes = cftp_client.getrecipe(ftp, target_file)
+    if hashes == None:
+        print "file not exist"
+    else:
+        print hashes
 
-if os.path.lexists(local_target_file):
-    ca.build_cache(local_target_file)
+def main():
+    if len(sys.argv) < 2:
+        print "command : ./recipe.py filename"
+    else:
+        filename = sys.argv[1]
+        ca = Chunk_Handler()
 
-hashes = cftp_client.getrecipe(ftp, target_file)
-if hashes == None:
-    print "file not exist"
-else:
-    print hashes
+        ftp = cftplib.FTP()
+        ftp.connect("localhost", 2121)
+        ftp.login("user", "12345")
+        check(ca, ftp, filename)
+
+if __name__ == "__main__":
+    main()
